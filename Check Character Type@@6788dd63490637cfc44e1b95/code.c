9cdea521204f0255dda667e8bdec9c49a616edf8
roshnee-loona <roshnee1643.becse24@chitkara.edu.in>
2025-01-31 11:29:24 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -3,8 +3,8 @@ int main() {
     char c;
     scanf("%c",&c);
     int a = (int)c;
-    if (a >= 96 && a<= 122){
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+    if ((a>= 65 && a<= 90) ||(a >= 96 && a<= 122)){
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E'||c == 'I' || c == 'O' || c == 'U')
     printf("Vowel");
     else 
     printf( "Consonant");}
